f7acfe8ebc7dd7f84aa74daffef31891c247b9f9
admin <admin@14a>
2019-02-13 12:32:00 +0700

    commit ke 1

--- InformationRetrieval/src/model/Document.java
+++ InformationRetrieval/src/model/Document.java
@@ -10,5 +10,43 @@ package model;
  * @author admin
  */
 public class Document {
+    private int id;
+    private String content;
+
+    public Document() {
+    }
+    
+    public Document(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @param content the content to set
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
     
 }
