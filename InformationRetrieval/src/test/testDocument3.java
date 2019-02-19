8201001b32c6fa2aba47703cbb014b3115206600
admin <admin@18c>
2019-02-19 12:55:36 +0700

    commit test doc3

---
+++ InformationRetrieval/src/test/testDocument3.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.ArrayList;
+import model.Document;
+import model.Posting;
+
+/**
+ *
+ * @author admin
+ */
+public class testDocument3 {
+
+    public static void main(String[] args) {
+
+        Document doc1 = new Document(1, "computer information retrieval");
+        Document doc2 = new Document(2, "computer organisation and architecture");
+        ArrayList<Document> listOfDocument = new ArrayList<Document>();
+        listOfDocument.add(doc1);
+        listOfDocument.add(doc2);
+
+        //siapkan posting list
+        ArrayList<Posting> list = new ArrayList<Posting>();
+        //buat node posting utk listOfDocument
+        for (int i = 0; i < listOfDocument.size(); i++) {
+            // buat listOfTerm dari document ke i
+            String[] termResult = listOfDocument.get(i).getListofTerm();
+            // loop sebanyak term dari document ke i
+            for (int j = 0; j < termResult.length; j++) {
+                // buat object termPosting
+                Posting tempPosting = new Posting(termResult[j],
+                        listOfDocument.get(i));
+                list.add(tempPosting);
+            }
+        }
+        //panggil list posting
+        System.out.println("Ukuran list = "+list.size());
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getTerm()+","+list.get(i).getDocument().getId());
+        }
+    }
+}
