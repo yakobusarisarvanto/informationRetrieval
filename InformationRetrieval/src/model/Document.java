6d047f2d79d18f84ed9a2be1f7d3d904e67621d4
admin <admin@26a>
2019-02-27 11:35:37 +0700

    buat posting list di kelas document

--- InformationRetrieval/src/model/Document.java
+++ InformationRetrieval/src/model/Document.java
@@ -5,6 +5,9 @@
  */
 package model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  *
  * @author admin
@@ -58,5 +61,49 @@ public class Document {
         String value = this.getContent();
         value = value.replaceAll("[.,?!]", "");
         return value.split(" ");
+    }
+
+    public ArrayList<Posting> getListofPosting() {
+        // panggil fungsi getListOfTerm
+        String tempString[] = getListofTerm();
+        // buat objek ArrayList<Posting> result untuk menampung hasil
+        ArrayList<Posting> result = new ArrayList<Posting>();
+        // buat looping sebanyak listOfTerm
+        for (int i = 0; i < tempString.length; i++) {
+            // di dalam looping
+            // jika term pertama maka
+            if (i == 0) {
+                // buat object tempPosting
+                Posting temPosting = new Posting(tempString[0], this);
+                // set atribut document, gunakan this
+                // tambahkan ke ArrayList result
+                result.add(temPosting);
+            } else {
+                // lainnya
+                // sorting ArayList result
+                Collections.sort(result);
+                // cek apakah term sudah ada
+                // gunakan fungsi search dengan luaran indeks obyek yang memenuhi
+                // buat object tempPosting           
+                Posting temPosting = new Posting(tempString[i], this);
+                int indexCari = Collections.binarySearch(result, temPosting);
+                // jika hasil cari kurang dari 0  (obyek tidak ada)
+                if (indexCari < 0) {
+                    // set atribut document, gunakan this
+                    // tambahkan ke ArrayList result
+                    result.add(temPosting);
+                } else {
+                    // lainnya   (obyek ada)
+                    // ambil postingnya, 
+                    // tambahkan atribut numberOfTerm dengan 1
+                    // dgn fungsi get
+                    // int tempNumber = get(indekshasilCari).getNumberOfTerm()+1;
+                    int tempNumber = result.get(indexCari).getNumberOfTerm() + 1;
+                    // atau get(indekshasilcari.setNumberOfTerm(tempNumber)
+                    result.get(indexCari).setNumberOfTerm(tempNumber);
+                }
+            }
+        }
+        return result;
     }
 }
