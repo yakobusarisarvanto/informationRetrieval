27a55c809728095d8edb3023854bfd12792582b9
admin <admin@28a>
2019-04-24 12:18:32 +0700

    project web srvice

---
+++ InformationRetrieval/src/testLucene/testIndonesiaStemmer.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testLucene;
+
+import org.apache.lucene.analysis.id.IndonesianStemmer;
+
+/**
+ *
+ * @author admin
+ */
+public class testIndonesiaStemmer {
+
+    public static void main(String[] args) {
+        IndonesianStemmer analyzer = new IndonesianStemmer();
+        String term = "mendukung";
+        char stemTerm[] = term.toCharArray();
+        int hasil = analyzer.stem(stemTerm, term.length(), true);
+        System.out.println(hasil);
+        System.out.println(stemTerm);
+        char stemResult[] = new char[hasil];
+        for (int i = 0; i < hasil; i++) {
+            stemResult[i] = stemTerm[i];
+        }
+        System.out.println(stemResult);
+    }
+}
